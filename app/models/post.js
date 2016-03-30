0d30e13635f2964ea8513131abbf441fc4f3d40d
brooke hurford and taylor pokoj <pokojt@gmail.com>
2016-03-30 09:11:19 -0700

    Created index route and post tile component, and they are connecting

---
+++ app/models/post.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  date: DS.attr(),
+  title: DS.attr(),
+  image: DS.attr(),
+  author: DS.attr(),
+  content: DS.attr(),
+  category: DS.attr()
+});
