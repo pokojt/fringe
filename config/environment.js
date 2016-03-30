661530312f381a1f7ad201082ba8014aaeb4a4f5
brooke hurford and taylor pokoj <pokojt@gmail.com>
2016-03-30 09:42:44 -0700

    Have content pulling from firebase and the post tile, and showing on the index page

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'fringe',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://fringe-blog.firebaseIO.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
